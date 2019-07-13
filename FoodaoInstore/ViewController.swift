fdb972475571116fa33437a532b9d08fbee6e0e0
simon.yunhong <stardaemon@163.com>
2019-07-13 10:36:58 +1000

    Initial Commit

---
+++ FoodaoInstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodaoInstore
+//
+//  Created by UNIVERSE on 13/7/19.
+//  Copyright © 2019 Yunhong Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
